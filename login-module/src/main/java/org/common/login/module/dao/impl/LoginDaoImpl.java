11ec4439a1e8db45d8573d839dd96318321eea29
ssadhukh <ssadhukh@cisco.com>
2018-03-01 01:54:08 +0530

    Added more config

--- login-module/src/main/java/org/common/login/module/dao/impl/LoginDaoImpl.java
+++ login-module/src/main/java/org/common/login/module/dao/impl/LoginDaoImpl.java
@@ -1,6 +1,13 @@
 package org.common.login.module.dao.impl;
 
 import java.util.List;
+import java.util.Queue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.common.login.module.dao.LoginDao;
 import org.common.login.module.entity.Login;
@@ -8,6 +15,8 @@ import org.common.login.utility.DBUtilityFactory;
 import org.hibernate.Session;
 
 public class LoginDaoImpl implements LoginDao {
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
 
 	@Override
 	public void insert(Login login) {
@@ -20,15 +29,33 @@ public class LoginDaoImpl implements LoginDao {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		finally{
+			session.close();
+		}
 		
-		session.close();
-		DBUtilityFactory.shutDown();
 	}
 
 	@Override
 	public List<Login> getAllLogin() {
-		// TODO Auto-generated method stub
-		return null;
+		//Session session = DBUtilityFactory.getSessionFacotry().openSession();
+		
+		/*session.beginTransaction();
+		Query query = session.createQuery("from Login");
+		List<Login> listOfLogin = query.getResultList();
+		session.close();*/
+		
+		EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        TypedQuery<Login> q = em.createQuery("from Login", Login.class);
+        List<Login> listOfLogin =  q.getResultList();
+        
+        
+        em.getTransaction().commit();
+        em.close();
+        
+        
+        
+		return listOfLogin;
 	}
 
 	@Override
--- login-module/src/test/java/org/common/login/module/login_module/AppTest.java
+++ login-module/src/test/java/org/common/login/module/login_module/AppTest.java
@@ -1,5 +1,7 @@
 package org.common.login.module.login_module;
 
+import java.util.List;
+
 import org.common.login.module.dao.LoginDao;
 import org.common.login.module.dao.impl.LoginDaoImpl;
 import org.common.login.module.entity.Login;
@@ -11,15 +13,26 @@ import org.junit.Test;
 public class AppTest 
 {
 
-	@Test
+	LoginDao loginDao = new LoginDaoImpl();
+	org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass().getName());
+	
+	
+	//@Test
 	public void insertLogin(){
 		Login login = new Login();
-		login.setUserName("saikat1");
-		login.setPassword("saikat1");
-		LoginDao loginDao = new LoginDaoImpl();
+		login.setUserName("saikat12");
+		login.setPassword("saikat5");
+		
 		loginDao.insert(login);
 	}
 	
-	
+	@Test
+	public void fetchListOfLogin() {
+		List<Login> listOfLogin = loginDao.getAllLogin();
+		for (Login login : listOfLogin) {
+			log.info("Id:"+ login.getId()+" User Name: "+login.getUserName());
+			//System.out.println("Id:"+ login.getId()+" User Name: "+login.getUserName());
+		}
+	}
 
 }
